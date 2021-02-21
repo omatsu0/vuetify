ea18086f751b46c297c13aaa43dfa63ef5c4d519
“sss-o2” <your@example.com>
2021-02-21 18:05:56 +0900

    両端揃え、右端揃えをEnterprise.vueに実装

---
+++ src/views/Enterprise.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <!-- 両端揃え -->
+    <v-row class="grey lighten-3 mt-5" justify="space-between" style="height: 200px;">
+      <v-col cols="3" style="background-color: #FFCDD2">
+        column 1
+      </v-col>
+      <v-spacer />
+      <!-- <v-col cols="auto" style="background-color: #F8BBD0">
+        column 2 column 2 column 2 column 2
+      </v-col> -->
+      <v-col cols="3" style="background-color: #E1BEE7">
+        column 3
+      </v-col>
+    </v-row>
+
+    <!-- 右端揃え -->
+    <v-row class="grey lighten-3 mt-5" justify="end" style="height: 200px;">
+      <v-col cols="3" style="background-color: #FFCDD2">
+        column 1
+      </v-col>
+      <!-- <v-spacer /> -->
+      <!-- <v-col cols="auto" style="background-color: #F8BBD0">
+        column 2 column 2 column 2 column 2
+      </v-col> -->
+      <v-col cols="3" style="background-color: #E1BEE7">
+        column 3
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
