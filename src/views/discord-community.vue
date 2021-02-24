29262817781b9cd960145693f78db59eeee8a813
“sss-o2” <your@example.com>
2021-02-25 00:39:29 +0900

    タイトル左btn右寄せ

--- src/views/discord-community.vue
+++ src/views/discord-community.vue
@@ -1,6 +1,5 @@
 <template>
   <v-container fluid pa-0 ma-0>
-    あああああ
     <v-row class="grey lighten-3 mt-5" justify="center" style="height: 200px;">
       <v-col cols="8" md="6" alignSelf="end" style="background-color: #FFCDD2">
         column 1
--- src/views/github-issue-board.vue
+++ src/views/github-issue-board.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="github">
-    <h1>This is an Github issue board page</h1>
-  </div>
+  <v-container fluid mr-5>
+    <v-row class="grey lighten-3" justify="space-between" style="height: 200px;">
+      <v-col cols="7" alignSelf="center" style="background-color: #FFCDD2">
+        <h1 :font-size="$vuetify.breakpoint.xs ? 20 : 5">タイトルタイ</h1>
+      </v-col>
+      <v-col style="background-color: #E1BEE7">
+        <v-btn color="primary">質問する</v-btn>
+        <!-- <v-btn color="primary" :min-height="$vuetify.breakpoint.xs ? 300 : '20vh'">質問する</v-btn> -->
+        <!-- <v-btn :block="$vuetify.breakpoint.xsOnly" flat>Cancel</v-btn> -->
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+<style scoped>
+h1{
+  font-size:1.5rem;
+}
+</style>
