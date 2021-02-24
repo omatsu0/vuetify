3b394f5e9389b335b2ed0994275d201517e75138
“sss-o2” <your@example.com>
2021-02-24 22:30:00 +0900

    alignSelf=end追加

--- src/views/discord-community.vue
+++ src/views/discord-community.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="discord">
-    <h1>This is an Discord community page</h1>
-  </div>
+  <v-container fluid pa-0 ma-0>
+    あああああ
+    <v-row class="grey lighten-3 mt-5" justify="center" style="height: 200px;">
+      <v-col cols="8" md="6" alignSelf="end" style="background-color: #FFCDD2">
+        column 1
+      </v-col>
+      <v-col cols="4" md="2" style="background-color: #E1BEE7">
+        column 3
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
