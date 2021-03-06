9285b52066ef608be8b8f6145009d71dbb22b0dc
“sss-o2” <your@example.com>
2021-03-07 00:01:36 +0900

    縦要素と横要素うまく並べた

--- src/views/github-issue-board.vue
+++ src/views/github-issue-board.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="container gray">
-    <div class="item blue">Item 1</div>
-    <div class="item red">Item 2</div>
-    <div class="item indigo">Item 3</div>
-    <div class="item pink">Item 4</div>
-    <span>aaa</span>
+    <div class="container2">
+      <div class="item blue">Item 1</div>
+      <div class="item red">Item 2</div>
+      <div class="item indigo">Item 3</div>
+      <div class="item pink">Item 4</div>
+    </div>
+    <div class="container3">
+      <div class="orange">あああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ</div>
+    </div>
   </div>
 </template>
 
@@ -12,22 +16,41 @@
 .gray{
   background-color: gray;
   display: flex;
-  flex-direction: column-reverse;
 }
 .blue{
   background-color: blue;
+  white-space: nowrap;
+  padding: 10px;
 }
 .red{
   background-color: red;
+  white-space: nowrap;
+  padding: 10px;
 }
 .indigo{
   background-color: indigo;
+  white-space: nowrap;
+  padding: 10px;
 }
 .pink{
   background-color: pink;
+  white-space: nowrap;
+  padding: 10px;
 }
 .inline-block{
   display: inline-block;
 }
+.container2{
+  display: flex;
+  flex-direction: column;
 
+}
+.container3{
+  display: flex;
+  width:inherit;
+  background-color: black;
+}
+.orange{
+  padding:10px;
+}
 </style>
