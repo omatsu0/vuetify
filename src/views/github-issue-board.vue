891b1c14ccdca708066332587168dbdd33675f35
“sss-o2” <your@example.com>
2021-03-06 22:53:17 +0900

    contaierでblock要素を囲んだだけのときは要素が縦並び

--- src/views/github-issue-board.vue
+++ src/views/github-issue-board.vue
@@ -1,37 +1,27 @@
 <template>
-  <v-container>
-    <v-col cols="1" class="indigo">
-      <v-row class="blue" justify="center" align="center">aaaa
-      </v-row>
-      <v-row class="red" justify="center" align="center">eeeee</v-row>
-      <v-row class="pink" justify="center" align="center">uuuu</v-row>
-    </v-col>
-    <v-col cols="8" class="cyan inline-block"><v-col class="pt-0">e---</v-col><v-col class="pb-3">e---</v-col><v-col class="pb-1">e---</v-col></v-col>
-
-  </v-container>
+  <div class="container gray">
+    <div class="item blue">Item 1</div>
+    <div class="item red">Item 2</div>
+    <div class="item indigo">Item 3</div>
+    <div class="item pink">Item 4</div>
+  </div>
 </template>
 
 <style>
+.gray{
+  background-color: gray;
+}
 .blue{
   background-color: blue;
-  width: 100px;
-  height: 50px;
 }
 .red{
   background-color: red;
-  width: 100px;
-  height: 50px;
 }
 .indigo{
-  height: 150px;
-  float:left;
+  background-color: indigo;
 }
 .pink{
   background-color: pink;
-  width: 100px;
-  text-align: center;
-  justify-content: center;
-  height: 50px;
 }
 .inline-block{
   display: inline-block;
